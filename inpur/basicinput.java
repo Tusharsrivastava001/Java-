c65e64a9ecc58f623f31772cc2e7f7a444f7ce91
Tushar srivastava <tusharsrivastava1725@gmail.com>
2026-08-13 02:07:07 +0530

    Add basic input example in basicinput.java

---
+++ inpur/basicinput.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+
+        
+        // Scanner sc=new Scanner(System.in);
+        // System.out.println("Enter the age: ");
+        // int age=sc.nextInt();
+        // System.out.println("Enter the name: ");
+        // String name=sc.next();
+        // System.out.println("Enter the ageii: ");
+        // double agei=sc.nextDouble();
+        // System.out.println(agei+" + "+age+name);
+        // System.out.println("Tushar");
+        // System.out.print("vivek this is the\ntum ");
+
+        int x=65;
+        char ch=(char)x;
+        System.out.println(ch);
+
+    }
+}
